aac4a5bbddafaa39f3dda3aa2cc6029ab706f1e5
JinFiZhou <33412369+fi94@users.noreply.github.com>
2021-09-28 09:49:17 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -61,5 +61,59 @@ def get_data_info_2w(data):
     data_dict['sentence_annotation'] = sentence_list
     return data_dict
 
+# 旧的一万张图片
+def get_info_1w(data,annotation_name,image_ann_path):
+    data_dict = {}
+    object_list = []
+    sentence_list = []
+    for ann in data['response']['annotations']:
+        if ann['type'] == 'document':
+            data_dict['news_type'] = ann['attributes']
+        else:
+            if ann['id'] == None:
+                one_object = {}
+                one_object['coordinate'] = {'width': ann['width'], 'height': ann['height']}
+                one_object['coordinate'].update(ann['vertices'][0])
+                one_object['object_label'] = ann['attributes']
+                object_list.append(one_object)
+            else:
+                sentence_list.append(ann['attributes'])
+    data_dict['object_annotation'] = object_list
+    data_dict['sentence_annotation'] = sentence_list
+    data_dict['image_name'] = annotation_name.split('.')[0].split('_')[0]
+    data_dict['annotation_path'] = image_ann_path
+    return data_dict
 
+if __name__ == '__main__':
+    args = parse_args()
+    # ======================================================================
+    # 第二批日常+突发新闻数据集
+    # data_list = read_json(args.data_root, json_file=args.json_dir)
+    # # print(len(data_list))
+    # data_info_list = []
+    # for data in data_list:
+    #     data_info = get_data_info(data)
+    #     data_info_list.append(data_info)
+    # print(len(data_info_list))
+    # with open(args.output_dir,'w') as f:
+    #     json.dump(data_info_list,f)
+    # =======================================================================
+    # 第一批突发数据集
+    # image_root = ['news_dataset/一万张第一批数据', 'news_dataset/一万张第二批数据']
+    annotation_root = ['news_dataset/新闻描述二期第一批数据', 'news_dataset/新闻描述二期第一批数据']
+    data_info_list = []
+    for annotation in annotation_root:
+        news_type_list = [type for type in os.listdir(annotation) if type.split('.')[-1] == 'txt']
+        for news_type in news_type_list:
+            news_type_path = os.path.join(annotation, news_type)
+            for image_ann in os.listdir(news_type_path):
+                image_ann_path = os.path.join(annotation, news_type, image_ann)
+                data = json.load(open(image_ann_path, 'r'))
+                data_info = get_info_1w(data,image_ann,image_ann_path)
+                data_info_list.append(data_info)
+
+    print(len(data_info_list))
+    print(data_info_list[0])
+    with open(args.output_dir,'w') as f:
+        json.dump(data_info_list,f)
 
